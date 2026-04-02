3d5d4721cb23e282225984a989e27975be4c167f
jasenc699-gif <jasenc699@gmail.com>
2026-04-03 08:11:51 +1300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fishinglog-v2';
+const CACHE = 'fishinglog-v3';
 const CORE = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => Promise.allSettled(CORE.map(a => c.add(a).catch(()=>{})))));
